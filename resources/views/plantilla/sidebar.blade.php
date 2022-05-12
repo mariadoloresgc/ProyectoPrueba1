db60945767da635d9cd8abf300443695b7a86830
LaMeery <mariadoloresgch@gmail.com>
2022-05-13 01:31:31 +0200

    Realizada Plantilla Principal Para Profesores - Commit 2

---
+++ resources/views/plantilla/sidebar.blade.php
@@ -0,0 +1,25 @@
+<ul class="metismenu" id="side-menu">
+
+    <li class="menu-title">Navigation</li>
+
+    <li>
+        <a href="widgets.html">
+            <i class="la la-dashboard"></i>
+            <span> Home </span>
+        </a>
+    </li>
+
+    <li>
+        <a href="widgets.html">
+            <i class="la la-ticket"></i>
+            <span> Incidents </span>
+        </a>
+    </li>
+
+    <li>
+        <a href="widgets.html">
+            <i class="la la-plus"></i>
+            <span> About Us </span>
+        </a>
+    </li>
+</ul>
